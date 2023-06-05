2e046bfe3d091e68722a94cd34fb56937d87a80e
Samyak Jain <jainsamyak2002.sj@gmail.com>
2023-06-05 13:49:47 +0530

    Added sample Player UI

--- app/screens/Player.js
+++ app/screens/Player.js
@@ -1,19 +1,72 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
 import React from 'react'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Slider from '@react-native-community/slider';
+
+import Screen from '../components/Screen';
+import color from '../misc/color';
+import PlayerButton from '../components/PlayerButton';
+
+const { width } = Dimensions.get('window');
 
 const Player = () => {
     return (
-        <View style={styles.container}>
-            <Text>Player</Text>
-        </View>
+        <Screen>
+            <View style={styles.container}>
+                <Text style={styles.audioCount}>1 / 99</Text>
+                <View style={styles.midBannerContainer}>
+                    <MaterialCommunityIcons name="music-circle" size={300} color={color.ACTIVE_BG} />
+                </View>
+
+                <View style={styles.audioPlayerContainer}>
+                    <Text numberOfLines={1} style={styles.audioTitle}>Audio File Name - Samyak Jain </Text>
+                </View>
+
+                <Slider
+                    style={{ width: width - 20, height: 40 }}
+                    minimumValue={0}
+                    maximumValue={1}
+                    minimumTrackTintColor={color.FONT_MEDIUM}
+                    maximumTrackTintColor={color.ACTIVE_BG}
+                />
+
+                <View style={styles.audioControllers}>
+                    <PlayerButton iconType='PREV' />
+                    <PlayerButton onPress={()=> console.log("Playing")} style={{ marginHorizontal: 50 }} iconType='PLAY' />
+                    <PlayerButton iconType='NEXT' />
+                </View>
+
+            </View>
+        </Screen>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    audioControllers: {
+        width,
+        flexDirection: 'row',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        paddingBottom: 20,
+    },
+    audioCount: {
+        textAlign: 'right',
+        padding: 15,
+        color: color.ACTIVE_BG,
+        fontSize: 14,
+    },
+    midBannerContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    audioTitle: {
+        fontSize: 16,
+        color: color.FONT,
+        padding: 15,
     }
 })
 
